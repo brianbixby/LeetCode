e9c9bd5849a811a7908e3fd2e9efecc5c6f1068e
BRIAN BIXBY <BRIANBIXBY0@GMAIL.COM>
2022-09-26 19:13:04 -0700

    2 sum sorted array

---
+++ two_sum/solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+ var twoSum = function(numbers, target) {
+    let start = 0;
+    let end = numbers.length - 1;
+  
+      while (start < end) {
+          const total = numbers[start] + numbers[end];
+          if (total == target) {
+              return [start + 1, end + 1];
+          }
+          total < target ? start++ : end--;
+      }
+  };
