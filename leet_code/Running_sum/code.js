0417254abd47dd9f1fb6609e5bf4d01c57954c2d
BRIAN BIXBY <BRIANBIXBY0@GMAIL.COM>
2022-10-26 10:41:35 -0700

    running sum

--- leet_code/Running_sum/code.js
+++ leet_code/Running_sum/code.js
@@ -1,7 +1,13 @@
+// faster than 99%
 var runningSum = function(nums) {
-    let arr = [];
-    for (let i=0;i<nums.length;i++) {
-        !i ? arr[i] = nums[i] : arr[i] = arr[i-1] + nums[i];
-    }
-    return arr;
+    return nums.reduce((acc, curr) => {
+        acc.total += curr;
+        acc.arr.push(acc.total);
+        return acc;
+    }, {arr: [], total: 0}).arr;
+    // let arr = [];
+    // for (let i=0;i<nums.length;i++) {
+    //     !i ? arr[i] = nums[i] : arr[i] = arr[i-1] + nums[i];
+    // }
+    // return arr;
 };
